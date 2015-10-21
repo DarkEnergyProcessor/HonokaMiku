0293db04d1033721fd8686b6a3887819c0336722
MikuAuahDark <auahdark687291@gmail.com>
2015-10-21 18:54:14 +0700

    Create EN_Decrypter.cc

---
+++ cpp_src/EN_Decrypter.cc
@@ -0,0 +1,120 @@
+/**
+* EN_Decrypter.cc
+* Decrypts SIF EN game files
+**/
+
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <cstring>
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
+#include "DecrypterContext.h"
+#include "Md5.h"
+
+EN_Dctx::EN_Dctx(const char* header,const char* filename)
+{
+	MD5_CTX* mctx;
+	const char* basename;
+	const char* basename2;
+	unsigned char magic_string[10];
+
+	basename=strrchr(filename,'/');
+	basename2=strrchr(filename,'\\');
+	if(basename==basename2)	// NULL==NULL = true
+		basename=filename;
+	else
+	{
+		basename=basename>basename2?basename:basename2;
+		basename++;
+	}
+
+	mctx=new MD5_CTX;
+	memcpy(magic_string,"BFd3EnkcKa",10);
+	MD5Init(mctx);
+	MD5Update(mctx,magic_string,10);
+	MD5Update(mctx,(unsigned char*)basename,strlen(basename));
+	MD5Final(mctx);
+
+	if(memcmp(header,mctx->digest+4,4))
+	{
+		delete mctx;
+		throw std::runtime_error(std::string("Header file doesn't match."));
+	}
+
+	memcpy(&init_key,mctx->digest,4);
+#ifdef _MSC_VER
+	init_key=_byteswap_ulong(init_key)&2147483647;
+#else
+	init_key=__builtin_bswap32(init_key)&2147483647;
+#endif
+	update_key=init_key;
+	xor_key=(init_key>>23)&255|((init_key>>15)&255)<<8;
+	pos=0;
+
+	delete mctx;
+}
+
+void EN_Dctx::decrypt_block(void* b,unsigned int size)
+{
+	if(size==0) return;
+
+	char* buffer=(char*)b;
+	if(pos%2==1)
+	{
+		buffer[0]^=char(xor_key>>8);
+		update();
+		pos++;
+		buffer++;
+	}
+
+	for(int i=0;i<size;i++)
+	{
+		if(i%2==0)
+			buffer[i]^=char(xor_key);
+		else
+		{
+			buffer[i]^=char(xor_key>>8);
+			update();
+		}
+		pos++;
+	}
+}
+
+void EN_Dctx::goto_offset(long offset)
+{
+	if(offset==0) return;
+
+	int x=pos+offset;
+	if(x<0) throw std::runtime_error(std::string("Position is negative."));
+
+	update_key=init_key;
+	xor_key=(init_key>>15)&255|((init_key>>23)&255)<<8;
+
+	if(x>1)
+		for(int i=1;i<=x/2;i++)
+			update();
+	pos=x;
+}
+
+void EN_Dctx::update()
+{
+	unsigned int a,b,c,d,e,f;
+
+	a=update_key;
+	b=a>>16;
+	c=(b*1101463552)&2147483647;
+	d=c+(a&65535)*16807;
+	e=(b*16807)>>15;
+	f=e+d-2147483647;
+	if(d>2147483646)
+		d=f;
+	else
+		d+=e;
+
+	update_key=d;
+	xor_key=(d>>23)&255|((d>>15)&255)<<8;
+}
