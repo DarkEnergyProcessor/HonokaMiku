90deb29dbe0b64f6f93566832942af22034f71fb
MikuAuahDark <auahdark687291@gmail.com>
2015-10-22 20:48:04 +0700

    Update md5.h

--- cpp_src/md5.h
+++ cpp_src/md5.h
@@ -38,7 +38,7 @@ typedef struct {
 } MD5_CTX;
 
 void MD5Init (MD5_CTX *mdContext);
-void MD5Update (MD5_CTX *mdContext, unsigned const char* inBuf, unsigned int inLen);
+void MD5Update (MD5_CTX *mdContext, const unsigned char* inBuf, unsigned int inLen);
 void MD5Final (MD5_CTX *mdContext);
 
 #ifdef __cplusplus
